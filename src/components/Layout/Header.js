e0e7a4697327cc9afa2d1dca9ebb369629f54003
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-28 11:58:18 +0800

    Update apps functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import meals from '../../assets/meals.avif'
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div>
             <header className={classes.header}>
                 <h1>MyFood</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={meals} alt="meals" />
